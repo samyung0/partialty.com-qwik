107fb2ee5d2139b1bed087f0575cb74d94076252
sam <yungchinpang999@gmail.com>
2023-08-26 12:26:06 +0800

    testing vercel

--- src/utils/supabaseServer.tsx
+++ src/utils/supabaseServer.tsx
@@ -1,12 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-import "dotenv/config";
+// import "dotenv/config";
 
 import type Supabase from "../types/Supabase";
 
 const SUPABASE_URL = process.env.SUPABASE_URL,
   SUPABASE_SECRET_KEY = process.env.SUPABASE_SECRET_KEY;
-
-// console.log(import.meta.env, "SUPABASE_SECRET_KEY: ", SUPABASE_SECRET_KEY);
 
 if (!SUPABASE_URL || !SUPABASE_SECRET_KEY)
   console.error("ENV VARIABLE ERROR SERVER");
