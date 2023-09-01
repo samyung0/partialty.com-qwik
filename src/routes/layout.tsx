ef208d7bcfe0e081b7f680fc273d0381427862c3
sam <yungchinpang999@gmail.com>
2023-09-01 18:13:59 +0800

    removed caching

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,13 +1,13 @@
 import { component$, Slot } from "@builder.io/qwik";
 import { type RequestHandler } from "@builder.io/qwik-city";
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // https://qwik.builder.io/docs/caching/
-  cacheControl({
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    maxAge: 5,
-  });
-};
+// export const onGet: RequestHandler = async ({ cacheControl }) => {
+//   // https://qwik.builder.io/docs/caching/
+//   cacheControl({
+//     staleWhileRevalidate: 60 * 60 * 24 * 7,
+//     maxAge: 5,
+//   });
+// };
 
 export default component$(() => {
   return <Slot />;
