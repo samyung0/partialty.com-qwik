61f017462e34b5b18d33ee5544baa9e2a1e37590
sam <yungchinpang999@gmail.com>
2023-09-07 19:34:33 +0800

    test vercel cache

--- src/routes/(wrapper)/about/index.tsx
+++ src/routes/(wrapper)/about/index.tsx
@@ -1,5 +1,13 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+  // https://qwik.builder.io/docs/caching/
+  cacheControl({
+    staleWhileRevalidate: 60 * 60 * 24 * 7,
+    maxAge: 60 * 60 * 24 * 7,
+  });
+};
 
 export default component$(() => {
   return <h1 class="h-screen">About Page</h1>;
