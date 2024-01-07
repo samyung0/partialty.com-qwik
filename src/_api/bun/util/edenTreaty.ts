18df7ba89f484f82f5e9987d428e9b953ebea421
samyung0 <yungchinpang999@gmail.com>
2024-01-07 14:55:11 +0800

    fix

--- src/_api/bun/util/edenTreaty.ts
+++ src/_api/bun/util/edenTreaty.ts
@@ -1,6 +1,6 @@
 import { edenTreaty } from '@elysiajs/eden'
 import type { App } from '../../../../bun/index' 
 
-const bunApp = edenTreaty<App>(import.meta.env.MODE === "development" ? "https://api.partialty.com" : 'http://localhost:8080')
+const bunApp = edenTreaty<App>(import.meta.env.MODE === "production" ? "https://api.partialty.com" : 'http://localhost:8080')
 
 export default bunApp;
