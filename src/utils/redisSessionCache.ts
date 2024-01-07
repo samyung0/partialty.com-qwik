657f688c7efefa8303fd873ed18c426d3502c1e0
samyung0 <yungchinpang999@gmail.com>
2024-01-07 14:20:22 +0800

    test

--- src/utils/redisSessionCache.ts
+++ src/utils/redisSessionCache.ts
@@ -1,9 +1,9 @@
-import { server$ } from "@builder.io/qwik-city";
-import { redis } from "~/utils/redis";
+// import { server$ } from "@builder.io/qwik-city";
+// import { redis } from "~/utils/redis";
 
-export const setCacheJson = server$(function (key: string, value: string) {
-  return redis(this).json.set(key, "$", value);
-});
-export const getCacheJson = server$(function (key: string) {
-  return redis(this).json.get(key);
-});
+// export const setCacheJson = server$(function (key: string, value: string) {
+//   return redis(this).json.set(key, "$", value);
+// });
+// export const getCacheJson = server$(function (key: string) {
+//   return redis(this).json.get(key);
+// });
