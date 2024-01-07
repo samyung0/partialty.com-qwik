35f718c9021e7e6fb60960a74d7bb87840d4fa4f
sam <yungchinpang999@gmail.com>
2024-01-07 11:43:39 +0800

    test

--- api/index.go
+++ api/index.go
@@ -8,13 +8,13 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	e := echo.New()
-	// e.GET("/", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello, World!")
-	// })
-	// e.GET("/test", func(c echo.Context) error {
-	// 	return c.String(http.StatusOK, "Hello, World This is test!")
-	// })
-	e.GET("/api/", func(c echo.Context) error {
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/test", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World This is test!")
+	})
+	e.GET("/api", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World This is api!")
 	})
 	e.ServeHTTP(w, r)
