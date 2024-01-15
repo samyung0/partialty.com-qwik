f7fdaefc19ff86195869799c247db1c4e7eb28b4
samyung0 <yungchinpang999@gmail.com>
2024-01-16 00:07:10 +0800

    fix

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,22 +1,11 @@
 /** @jsxImportSource react */
 import { codeInput } from "@sanity/code-input";
-import { DashboardIcon } from "@sanity/icons";
 import { table } from "@sanity/table";
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { media } from "sanity-plugin-media";
 import { structureTool } from "sanity/structure";
-import { CardTool } from "./sanity/components/Card";
 import { schema } from "./sanity/schemas";
-
-const myCustomTool = () => {
-  return {
-    title: "My Custom Tool",
-    name: "my-custom-tool", // localhost:3333/my-custom-tool
-    icon: DashboardIcon,
-    component: CardTool,
-  };
-};
 
 export default defineConfig({
   name: "default",
@@ -25,11 +14,35 @@ export default defineConfig({
   projectId: "4psl6aio",
   dataset: "production",
 
-  plugins: [codeInput(), structureTool(), visionTool(), table(undefined), media()],
+  plugins: [
+    codeInput(),
+    structureTool({
+      structure: (S, context) => {
+        console.log(context);
+        return S.list()
+          .title("Content")
+          .items([
+            S.listItem()
+              .title("Author")
+              .child(
+                S.editor().schemaType("author").documentId("8a4e550a-1566-4871-89a2-12e4aa2ecc39")
+              ),
+            // Add a visual divider (optional)
+            S.divider(),
+            // List out the rest of the document types, but filter out the config type
+            ...S.documentTypeListItems().filter(
+              (listItem) => !["author"].includes(listItem.getId())
+            ),
+          ]);
+      },
+    }),
+    visionTool(),
+    table(undefined),
+    media(),
+  ],
   // document: {
   //   actions: (prev, context) =>{
   //     return prev.map((originalAction) => (originalAction.action === 'publish' ? SetAndPublishAction : originalAction)) as any}
   // },
-  tools: [myCustomTool()],
   schema,
 });
--- sanity/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-/** @jsxImportSource react */
-import { Card, Text } from "@sanity/ui";
-export function CardTool() {
-  return (
-    <Card padding={4}>
-      <Text>My custom tool!</Text>
-    </Card>
-  );
-}
