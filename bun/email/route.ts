75ce85614f91889a5c5fa6c43b127266ccdb535a
samyung0 <yungchinpang999@gmail.com>
2024-01-16 18:06:08 +0800

    test

--- bun/email/route.ts
+++ bun/email/route.ts
@@ -14,34 +14,38 @@ const senderMail = "automatic@partialty.com";
 const senderName = "Partialty";
 
 const app = new Elysia().group("/mail", (app) => {
-  return app.post(
-    "/sendMail/verifyMail",
-    async ({ body }) => {
-      const res = eta.render("verifyEmail.eta", {
-        verifyLink: body.verifyLink,
-      });
-      let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-      sendSmtpEmail.subject = "Verify Your Partialty Account";
-      sendSmtpEmail.htmlContent = res;
-      sendSmtpEmail.sender = { email: senderMail, name: senderName };
-      sendSmtpEmail.to = [{ email: body.receiverEmail }];
-      await apiInstance.sendTransacEmail(sendSmtpEmail).then(
-        (data) => {
-          console.log("API called successfully. Returned data: " + data.body.messageId);
-        },
-        (error) => {
-          throw new Error("Server Error! ", error);
-        }
-      );
-      return { message: "OK" };
-    },
-    {
-      body: t.Object({
-        verifyLink: t.String(),
-        receiverEmail: t.String(),
-      }),
-    }
-  );
+  return app
+    .post("/test", ({ headers }) => {
+      console.log(headers);
+    })
+    .post(
+      "/sendMail/verifyMail",
+      async ({ body }) => {
+        const res = eta.render("verifyEmail.eta", {
+          verifyLink: body.verifyLink,
+        });
+        let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+        sendSmtpEmail.subject = "Verify Your Partialty Account";
+        sendSmtpEmail.htmlContent = res;
+        sendSmtpEmail.sender = { email: senderMail, name: senderName };
+        sendSmtpEmail.to = [{ email: body.receiverEmail }];
+        await apiInstance.sendTransacEmail(sendSmtpEmail).then(
+          (data) => {
+            console.log("API called successfully. Returned data: " + data.body.messageId);
+          },
+          (error) => {
+            throw new Error("Server Error! ", error);
+          }
+        );
+        return { message: "OK" };
+      },
+      {
+        body: t.Object({
+          verifyLink: t.String(),
+          receiverEmail: t.String(),
+        }),
+      }
+    );
 });
 
 export default app;
