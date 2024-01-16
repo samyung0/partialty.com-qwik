0de528da2e630925d5c4236581de87a17123a65b
samyung0 <yungchinpang999@gmail.com>
2024-01-16 09:00:10 +0800

    removed sanity

--- sanity/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import author from "./author";
-import embed from "./embed";
-import post from "./post";
-import tag from "./tag";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, embed, author, tag],
-};
