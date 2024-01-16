8d7d3e382870ec4c1c1aab322fc71c5ccb0eb444
samyung0 <yungchinpang999@gmail.com>
2024-01-16 20:24:27 +0800

    switch to resend

--- bun/email/route.ts
+++ bun/email/route.ts
@@ -1,19 +1,21 @@
 import { Elysia, t } from "elysia";
 
-import SibApiV3Sdk from "@getbrevo/brevo";
+// import SibApiV3Sdk from "@getbrevo/brevo";
 import { Receiver } from "@upstash/qstash";
 import { Eta } from "eta";
 import path from "node:path";
+import { Resend } from "resend";
 
 if (
-  !Bun.env.BREVO_API_KEY ||
+  !Bun.env.RESEND_API_KEY ||
+  // !Bun.env.BREVO_API_KEY ||
   !Bun.env.QSTASH_CURRENT_SIGNING_KEY ||
   !Bun.env.QSTASH_NEXT_SIGNING_KEY
 )
   throw new Error("Server env var Error!");
-let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-let apiKey = (apiInstance as any).authentications["apiKey"];
-apiKey.apiKey = Bun.env.BREVO_API_KEY;
+// let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+// let apiKey = (apiInstance as any).authentications["apiKey"];
+// apiKey.apiKey = Bun.env.BREVO_API_KEY;
 const eta = new Eta({ views: path.resolve(import.meta.dir, "./templates") });
 
 const senderMail = "automatic@partialty.com";
@@ -48,30 +50,30 @@ const app = new Elysia().group("/mail", (app) => {
       const res = eta.render("verifyEmail.eta", {
         verifyLink: content.verifyLink,
       });
-      // const resend = new Resend(Bun.env.RESEND_API_KEY!);
+      const resend = new Resend(Bun.env.RESEND_API_KEY!);
 
-      // resend.emails.send({
-      //   from: "automatic@partialty.com",
-      //   to: content.receiverEmail,
-      //   subject: "Verify Your Partialty Account",
-      //   html: res,
-      // });
+      resend.emails.send({
+        from: "automatic@partialty.com",
+        to: content.receiverEmail,
+        subject: "Verify Your Partialty Account",
+        html: res,
+      });
 
-      let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-      sendSmtpEmail.subject = "Verify Your Partialty Account";
-      sendSmtpEmail.htmlContent = res;
-      sendSmtpEmail.sender = { email: senderMail, name: senderName };
-      sendSmtpEmail.to = [{ email: content.receiverEmail }];
-      await apiInstance.sendTransacEmail(sendSmtpEmail).then(
-        (data) => {
-          console.log(
-            "Verification Email Sent Successfully. Returned data: " + data.body.messageId
-          );
-        },
-        (error) => {
-          throw new Error("Server Error! ", error);
-        }
-      );
+      // let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+      // sendSmtpEmail.subject = "Verify Your Partialty Account";
+      // sendSmtpEmail.htmlContent = res;
+      // sendSmtpEmail.sender = { email: senderMail, name: senderName };
+      // sendSmtpEmail.to = [{ email: content.receiverEmail }];
+      // await apiInstance.sendTransacEmail(sendSmtpEmail).then(
+      //   (data) => {
+      //     console.log(
+      //       "Verification Email Sent Successfully. Returned data: " + data.body.messageId
+      //     );
+      //   },
+      //   (error) => {
+      //     throw new Error("Server Error! ", error);
+      //   }
+      // );
 
       return {
         statusCode: 200,
