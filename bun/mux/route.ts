6378b544d99c73f20d2845aa8ebdbd09f0d0f947
samyung0 <yungchinpang999@gmail.com>
2024-01-27 11:09:08 +0800

    test mux

--- bun/mux/route.ts
+++ bun/mux/route.ts
@@ -1,3 +1,4 @@
+import { createHmac } from "crypto";
 import Elysia, { t } from "elysia";
 
 if (!Bun.env.MUX_SIGNING_SECRET) throw new Error("Server MUX env var Error!");
@@ -6,18 +7,18 @@ const app = new Elysia().group("/mux", (app) => {
   return app.post(
     "/",
     async ({ body, headers }) => {
-      // if (!headers["Mux-Signature"]) {
-      //   throw new Error("Invalid signature!");
-      // }
+      if (!headers["mux-signature"]) {
+        throw new Error("Invalid signature!");
+      }
       console.log(headers, body);
-      // const [_t, _v1] = headers["Mux-Signature"];
-      // const t = _t.slice(2);
-      // const expected_signature = _v1.slice(3);
-      // const payload = t + "." + body;
-      // const hmac = createHmac("sha256", Bun.env.MUX_SIGNING_SECRET!)
-      //   .update(payload)
-      //   .digest("base64");
-      // console.log(hmac, expected_signature, hmac === expected_signature);
+      const [_t, _v1] = headers["mux-signature"];
+      const t = _t.slice(2);
+      const expected_signature = _v1.slice(3);
+      const payload = t + "." + body;
+      const hmac = createHmac("sha256", Bun.env.MUX_SIGNING_SECRET!)
+        .update(payload)
+        .digest("base64");
+      console.log(hmac, expected_signature, hmac === expected_signature);
     },
     {
       async parse(ctx) {
