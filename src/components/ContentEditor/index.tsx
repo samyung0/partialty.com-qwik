590619c89dde3aecc8c8f3f0f915f70da9f7ff41
samyung0 <yungchinpang999@gmail.com>
2024-02-03 01:24:39 +0800

    remove comment

--- src/components/ContentEditor/index.tsx
+++ src/components/ContentEditor/index.tsx
@@ -94,57 +94,6 @@ const schema: ListsSchema = {
     return { children: [{ text: "" }], ...props, type: "list-item-text" };
   },
 };
-
-// const initialValue: Descendant[] = [
-//   {
-//     type: "paragraph",
-//     children: [
-//       {
-//         text: "",
-//       },
-//       {
-//         type: "link",
-//         url: "https://google.com",
-//         children: [{ text: "a" }],
-//       },
-//       { text: "" },
-//     ],
-//   },
-//   {
-//     type: "paragraph",
-//     children: [
-//       { text: "This is editable " },
-//       { text: "rich", bold: true },
-//       { text: " text, " },
-//       { text: "much", italic: true },
-//       { text: " better than a " },
-//       { text: "<textarea>", code: true },
-//       { text: "!" },
-//     ],
-//   },
-//   {
-//     type: "paragraph",
-//     children: [
-//       {
-//         text: "Since it's rich text, you can do things like turn a selection of text ",
-//       },
-//       { text: "bold", bold: true },
-//       {
-//         text: ", or add a semantically rendered block quote in the middle of the page, like this:",
-//       },
-//     ],
-//   },
-//   {
-//     type: "block-quote",
-//     children: [{ text: "A wise quote." }],
-//   },
-//   {
-//     type: "paragraph",
-//     align: "center",
-//     children: [{ text: "Try it out for yourself!" }],
-//   },
-// ];
-
 const ContentEditorReact = ({
   isPreviewing,
   setIsPreviewing,
