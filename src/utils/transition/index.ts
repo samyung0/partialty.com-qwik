93c15c1a6a08bc805eca12e1c10cab043ec9e1b2
samyung0 <yungchinpang999@gmail.com>
2024-02-28 19:16:45 +0800

    update

--- src/utils/transition/index.ts
+++ src/utils/transition/index.ts
@@ -1,7 +1,9 @@
 import easeInQuart from "~/utils/transition/easeInQuart";
+import easeInQuint from "~/utils/transition/easeInQuint";
 import easeInSine from "~/utils/transition/easeInSine";
 
 export default {
   easeInSine,
   easeInQuart,
+  easeInQuint
 } as const;
