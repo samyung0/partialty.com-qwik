d8faf5e24fe5f61128744e96b1a6e9d24c11f89e
samyung0 <yungchinpang999@gmail.com>
2024-03-15 10:05:31 +0800

    switch to netlify

--- astro_embed/astro.config.mjs
+++ astro_embed/astro.config.mjs
@@ -2,22 +2,16 @@ import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-
 import qwikdev from "@qwikdev/astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    solidJs(),
-    svelte(),
-    qwikdev(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), solidJs(), svelte(), qwikdev()],
   output: "server",
-  adapter: vercel(),
+  adapter: netlify()
 });
