e10d3d265bef0d2db2714b756c3c76a7a48a329f
samyung0 <yungchinpang999@gmail.com>
2024-04-01 19:46:00 +0800

    fix leading

--- src/components/Prose/react-prose.tsx
+++ src/components/Prose/react-prose.tsx
@@ -52,14 +52,14 @@ const Prose = ({
       // SHOULD add a background
       id="sectionProse"
       className="prose mx-auto w-[90%] max-w-[unset] bg-background-light-gray px-4 py-6 text-sm
-    prose-a:decoration-wavy prose-a:underline-offset-4
-    prose-blockquote:pr-[1em]
-    prose-figcaption:mt-0 prose-figcaption:text-xs
-    prose-figcaption:italic
-    prose-strong:tracking-wider prose-pre:bg-code-editor-one-dark-pro prose-pre:font-cascadiaCode  prose-pre:text-sm
-    prose-pre:leading-5
+    leading-6 prose-a:decoration-wavy
+    prose-a:underline-offset-4
+    prose-blockquote:pr-[1em] prose-figcaption:mt-0 prose-figcaption:text-xs prose-figcaption:italic
+    prose-strong:tracking-wider
+    prose-pre:bg-code-editor-one-dark-pro prose-pre:font-cascadiaCode prose-pre:text-sm  prose-pre:leading-5
     prose-img:m-0
-    dark:bg-primary-dark-gray dark:text-background-light-gray dark:prose-figcaption:text-gray-300 md:px-10 md:py-12 lg:text-base lg:prose-figcaption:text-sm lg:prose-pre:text-base lg:prose-pre:leading-6 [&_code:not(pre_code)]:rounded-md [&_code:not(pre_code)]:bg-gray-300/70 [&_code:not(pre_code)]:p-[2px] [&_code:not(pre_code)]:text-[unset]  [&_code:not(pre_code)]:before:content-none [&_code:not(pre_code)]:after:content-none dark:[&_code:not(pre_code)]:bg-gray-600/70
+    dark:bg-primary-dark-gray
+    dark:text-background-light-gray dark:prose-figcaption:text-gray-300 md:px-10 md:py-12 lg:text-base lg:leading-7 lg:prose-figcaption:text-sm lg:prose-pre:text-base lg:prose-pre:leading-6 [&_code:not(pre_code)]:rounded-md [&_code:not(pre_code)]:bg-gray-300/70 [&_code:not(pre_code)]:p-[2px] [&_code:not(pre_code)]:text-[unset]  [&_code:not(pre_code)]:before:content-none [&_code:not(pre_code)]:after:content-none dark:[&_code:not(pre_code)]:bg-gray-600/70
     "
       dangerouslySetInnerHTML={{ __html: innerHTML }}
     ></article>
@@ -67,15 +67,15 @@ const Prose = ({
     <section
       // SHOULD add a background
       className="prose mx-auto h-[80vh] 
-  max-h-[80vh] w-full max-w-[unset] overflow-auto bg-background-light-gray px-4 py-6 text-sm prose-a:decoration-wavy prose-a:underline-offset-4
-  prose-blockquote:pr-[1em] prose-figcaption:mt-0
-  prose-figcaption:text-xs
-  prose-figcaption:italic prose-strong:tracking-wider
-  prose-pre:bg-code-editor-one-dark-pro
-  prose-pre:font-cascadiaCode prose-pre:text-sm prose-pre:leading-5  prose-img:m-0 dark:bg-primary-dark-gray dark:text-background-light-gray
-  dark:prose-figcaption:text-gray-300
+  max-h-[80vh] w-full max-w-[unset] overflow-auto bg-background-light-gray px-4 py-6 text-sm leading-6 prose-a:decoration-wavy
+  prose-a:underline-offset-4 prose-blockquote:pr-[1em]
+  prose-figcaption:mt-0
+  prose-figcaption:text-xs prose-figcaption:italic prose-strong:tracking-wider prose-pre:bg-code-editor-one-dark-pro
+  prose-pre:font-cascadiaCode
+  prose-pre:text-sm prose-pre:leading-5 prose-img:m-0  dark:bg-primary-dark-gray dark:text-background-light-gray dark:prose-figcaption:text-gray-300
   md:px-10
-  md:py-12 lg:text-base lg:prose-figcaption:text-sm lg:prose-pre:text-base lg:prose-pre:leading-6 [&_code:not(pre_code)]:rounded-md [&_code:not(pre_code)]:bg-gray-300/70 [&_code:not(pre_code)]:p-[2px] [&_code:not(pre_code)]:text-[unset]  [&_code:not(pre_code)]:before:content-none [&_code:not(pre_code)]:after:content-none dark:[&_code:not(pre_code)]:bg-gray-600/70
+  md:py-12
+  lg:text-base lg:leading-7 lg:prose-figcaption:text-sm lg:prose-pre:text-base lg:prose-pre:leading-6 [&_code:not(pre_code)]:rounded-md [&_code:not(pre_code)]:bg-gray-300/70 [&_code:not(pre_code)]:p-[2px] [&_code:not(pre_code)]:text-[unset]  [&_code:not(pre_code)]:before:content-none [&_code:not(pre_code)]:after:content-none dark:[&_code:not(pre_code)]:bg-gray-600/70
   "
     >
       {children}
