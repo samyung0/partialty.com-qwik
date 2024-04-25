266e425e1a528c6e199552acafba8257d912bbe9
samyung0 <yungchinpang999@gmail.com>
2024-04-25 14:11:23 +0800

    switched to google run

---
+++ adapters/cloud-run/vite.config.ts
@@ -0,0 +1,23 @@
+import { cloudRunAdapter } from '@builder.io/qwik-city/adapters/cloud-run/vite';
+import { extendConfig } from '@builder.io/qwik-city/vite';
+import baseConfig from '../../vite.config';
+
+export default extendConfig(baseConfig, () => {
+  return {
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ['src/entry.cloud-run.tsx', '@qwik-city-plan'],
+      },
+    },
+    plugins: [
+      cloudRunAdapter({
+        ssg: {
+          include: ['/courses/*'],
+          origin: 'https://www.partialty.com',
+          sitemapOutFile: 'sitemap-ssg.xml',
+        },
+      }),
+    ],
+  };
+});
